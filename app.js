57ddbdba4dcbac74c82d87d9720b5fd9e0bb4385
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-02 16:56:29 +0300

    GET And POST

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use("/add-product",(req,res,next)=>{
 
 
 
-app.use("/product",(req,res,next)=>{
+app.post("/product",(req,res,next)=>{
     console.log(req.body);
 res.redirect('/')
 })
 
 
 
-app.use("/",(req,res,next)=>{
-    res.send("<h1>main page</h1>");
+app.get("/",(req,res,next)=>{
+    res.send("<h1>Main page</h1>");
     ;
 })
 
