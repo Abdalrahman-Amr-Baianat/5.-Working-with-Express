a33a8f6fe84bf5e27a79d9d2a5fee529fe9f4058
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-02 17:19:14 +0300

    Using Express Router

--- app.js
+++ app.js
@@ -3,40 +3,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express()
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded())
-
-
-
-
-app.use("/",(req,res,next)=>{
-    console.log("This always Runs");
-    next();
-})
-
-app.use("/add-product",(req,res,next)=>{
-    res.send('<form action ="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>');
-
-})
+app.use(bodyParser.urlencoded());
 
 
 
 
+app.use(shopRoutes)
+app.use(adminRoutes)
 
 
 
-
-app.post("/product",(req,res,next)=>{
-    console.log(req.body);
-res.redirect('/')
-})
-
-
-
-app.get("/",(req,res,next)=>{
-    res.send("<h1>Main page</h1>");
-    ;
-})
 
 
 
