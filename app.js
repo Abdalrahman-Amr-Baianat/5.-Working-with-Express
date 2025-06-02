0cd0630edc36940d06fb54892fa68a3aea057b74
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-02 14:41:01 +0300

    test1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes')
+const app = express()
 
-const server = http.createServer(routes.handler);
+app.use((req,res,next)=>{
+    console.log("In the middle ware");
+})
+const server = http.createServer(app);
 
 server.listen(3000);
 
