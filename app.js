c003e443dad5f5c04d465ae1cf684fcb53eec7c9
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-02 16:01:44 +0300

    pefore parsing

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const http = require('http');
+
 const express = require('express');
 
 const app = express()
-
-app.use((req,res,next)=>{
-    console.log("In the middle ware");
+app.use("/",(req,res,next)=>{
+    res.send("<h1>main page</h1>");
 })
-const server = http.createServer(app);
 
-server.listen(3000);
 
+app.use("/add-product",(req,res,next)=>{
+    res.send("<h1>Product</h1>");
+})
+
+
+
+
+
+
+
+app.listen(3000)
