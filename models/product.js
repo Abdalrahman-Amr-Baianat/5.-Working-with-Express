e0e5fae8b3409fef936a6d84c25e41b34987d673
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-16 11:01:01 +0300

    loading product detailes data

--- models/product.js
+++ models/product.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -23,18 +27,22 @@ module.exports = class Product {
 
   save() {
     this.id = Math.random().toString();
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        if (err) {
-          console.log(err, '<<<<<<<<<<<<');
-        }
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
-    fs.readFile(p, (err, fileContent) => {});
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
