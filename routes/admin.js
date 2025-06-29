43e4c8857ee7eb2bb33b7f8db2a940b3ce71433f
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-29 18:49:16 +0300

    Refactoring with mongoose

--- routes/admin.js
+++ routes/admin.js
@@ -3,21 +3,20 @@ const path = require("path");
 const express = require("express");
 
 const adminController = require("../controllers/admin");
-const { post } = require("./shop");
 
 const router = express.Router();
 
 router.get("/add-product", adminController.getAddProduct);
 
-// /admin/products => GET
+// // /admin/products => GET
 router.get("/products", adminController.getProducts);
 
-router.post("/add-product", adminController.postAddProduct);
+ router.post("/add-product", adminController.postAddProduct);
 
- router.get("/edit-product/:productId", adminController.getEditProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
- router.post("/edit-product", adminController.postEditProduct);
+//  router.post("/edit-product", adminController.postEditProduct);
 
-router.post("/delete-product", adminController.postDeleteProduct);
+// router.post("/delete-product", adminController.postDeleteProduct);
 
-module.exports = router;
+ module.exports = router;
