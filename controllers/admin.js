bae378572c123a06d0f120fe6838856aaa3333ef
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-06-29 18:56:08 +0300

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const product = require("../models/product");
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -61,15 +62,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+
+      product.save();
+    })
+
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
